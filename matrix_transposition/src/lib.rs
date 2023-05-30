cec632076875e2ee189e88f592912c15606ad653
Svalbard <svalbardmalfant@gmail.com>
2023-05-30 08:27:18 +0200

    matrix derive

--- matrix_transposition/src/lib.rs
+++ matrix_transposition/src/lib.rs
@@ -1,7 +1,6 @@
 
 // #debug
-#[derive(Debug)]
-
+#[derive(Debug, PartialEq, Eq)]
 
 pub struct Matrix(pub(i32, i32), pub(i32, i32));
 
@@ -9,7 +8,6 @@ pub fn transpose(m: Matrix) -> Matrix {
     let Matrix((a, b), (c, d)) = m;
     Matrix((a, c), (b, d))
 }
-
 
 #[cfg(test)]
 mod tests {
