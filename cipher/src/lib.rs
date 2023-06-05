7d7640a982838da53b9ae37c063f354d3f1e1536
Svalbard <svalbardmalfant@gmail.com>
2023-06-05 19:27:29 +0200

    cipher

---
+++ cipher/src/lib.rs
@@ -0,0 +1,42 @@
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct CipherError {
+    // expected public fields
+    pub validation : bool,
+    pub expected : String,
+}
+impl CipherError {
+    pub fn new(validation: bool, expected: String) -> CipherError {
+        CipherError{
+            validation : validation,
+            expected : expected,
+        }
+    }
+}
+pub fn cipher(original: &str, ciphered: &str) -> Option<Result<bool, CipherError>> {
+    if original != "" {
+        
+    let mut cipher_test = String::new();
+
+    for c in original.chars() {
+        if c.is_alphabetic() {
+            if c.is_uppercase() {
+                //let mut ascii_original = c as u8;
+                let ascii_ciphered = 'Z' as u8 - c as u8 + 'A' as u8;
+                cipher_test.push(ascii_ciphered as char);
+            } else {
+                //let mut ascii_original = c as u8;
+                let ascii_ciphered = 'z' as u8 - c as u8 + 'a' as u8;
+                cipher_test.push(ascii_ciphered as char);
+            }
+        } else {
+            cipher_test.push(c);
+        }
+    }
+    if cipher_test == ciphered {
+        return Some(Ok(true));
+    } else {
+        return Some(Err(CipherError::new(false, cipher_test.to_string())));
+    }
+}
+return None;
+}
---
+++ cipher/src/main.rs
@@ -0,0 +1,8 @@
+use cipher::*;
+
+fn main() {
+    println!("{:?}", cipher("1Hello 2world!", "1Svool 2dliow!"));
+    println!("{:?}", cipher("1Hello 2world!", "svool"));
+    println!("{:?}", cipher("", "svool"));
+    
+}
