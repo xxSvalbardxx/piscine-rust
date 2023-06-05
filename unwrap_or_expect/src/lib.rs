816e1b5fdd1e1da3465dbca1289f4e155499aac9
Svalbard <svalbardmalfant@gmail.com>
2023-06-05 11:43:36 +0200

    unwrap_or_expect

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -10,10 +10,8 @@ pub fn fetch_data(server: Result<String, String>, security_level: Security) -> S
     server.clone().unwrap_or_else(|err| match security_level {
         Security::Unknown => return server.unwrap(),
         Security::High => return  server.unwrap_or("ERROR: program stops".to_string()),
-        Security::Medium => return server.unwrap_or("WARNING: check the server".to_string()),
-        
+        Security::Medium => return server.unwrap_or("WARNING: check the server".to_string()),        
         Security::Low => return server.unwrap_or("Not found: ".to_string() + &err),
-        // Security::BlockServer will panic with the method unwrap_err() and the message "Not found: malicious_server.com"
         Security::BlockServer => return server.unwrap_err(),
     })
 }
