399f54db50a2631b381ee492f4e15143ced90e2b
Svalbard <svalbardmalfant@gmail.com>
2023-06-05 14:14:52 +0200

    profanity_filter

--- profanity_filter/src/lib.rs
+++ profanity_filter/src/lib.rs
@@ -1,28 +1,25 @@
-pub struct Message {    
+pub struct Message {
     content: String,
-    user: String,
+    user : String,
 }
 
 impl Message {
   pub fn new(ms: String, u: String) -> Message {
-    Message {
-      content: ms,
-      user: u,
-    }
-
+    Message { ms, u }
   }
   pub fn send_ms(&self) -> Option<&str> {
     if self.content.len() == 0 || self.content == "stupid" {
-        None
+      None
     } else {
-        Some(&self.content)
+      Some(&self.content)
     }
   }
 }
 
 pub fn check_ms(ms: &Message) -> (bool, &str) {
-    if ms.send_ms() == None{
-        return (false,"ERROR : illegal");
+    match ms.send_ms() {
+        Some(ms) => (true, ms),
+        None => (false, "ERROR : illegal"),
     }
-    (true,"")
+
 }
