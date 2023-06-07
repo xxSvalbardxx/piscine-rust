7cd3e654ff59f4ec88a8fc9bcc56d4a0f29ab8bb
Svalbard <svalbardmalfant@gmail.com>
2023-06-07 09:50:35 +0200

    middle_day

---
+++ middle_day/src/lib.rs
@@ -0,0 +1,15 @@
+use chrono::Weekday as wd;
+use chrono::NaiveDate;
+use chrono::Datelike;
+
+
+// this function will return the middle day of the year passed as argument or an error
+pub fn middle_day(year: i32) -> Option<wd> {
+    if NaiveDate::from_ymd_opt(year, 2, 29).is_some() {
+        return None;
+    } else {
+        // find the week day of the 2nd of July of the year passed as argument
+        return Some(NaiveDate::from_ymd_opt(year, 7, 2).unwrap().and_hms_opt(0, 0, 0).unwrap().weekday());
+    }
+    
+}
---
+++ middle_day/src/main.rs
@@ -0,0 +1,5 @@
+use middle_day::*;
+
+fn main() {
+    println!("{:?}", middle_day(1022).unwrap());
+}
