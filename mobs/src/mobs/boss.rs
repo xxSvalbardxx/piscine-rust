129f85b63c027142abf3d1f10fddf480a17ac516
Svalbard <svalbardmalfant@gmail.com>
2023-06-08 16:31:54 +0200

    mobs

--- mobs/src/mobs/boss.rs
+++ mobs/src/mobs/boss.rs
@@ -1,17 +1,17 @@
 use mobs::*;
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct Boss {
-    name: String,
-    age : u8,
-}
-
-impl Boss {
-    pub fn new(name: String, age: u8) -> Boss {
-        Boss {
-            name: name.to_string(),
-            age: age,
-        }
+pub mod boss {
+    #[derive(Debug, Clone, PartialEq)]
+    pub struct Boss {
+        name: String,
+        age: u8,
     }
 
+    impl Boss {
+        pub fn new(name: String, age: u8) -> Boss {
+            Boss {
+                name: name.to_string(),
+                age: age,
+            }
+        }
+    }
 }
--- mobs/src/mobs/member.rs
+++ mobs/src/mobs/member.rs
@@ -1,27 +1,28 @@
 use mobs::*;
+pub mod member {
+    #[derive(Debug, Clone, PartialEq)]
+    pub enum Role {
+        Underboss,
+        Caporegime,
+        Soldier,
+        Associate,
+    }
 
-#[derive(Debug, Clone, PartialEq)]
-pub enum Role {
-    Underboss,
-    Caporegime,
-    Soldier,
-    Associate,
-}
+    #[derive(Debug, Clone, PartialEq)]
+    pub struct Member {
+        name: String,
+        role: Role,
+        age: u8,
+    }
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct Member {
-    name: String,
-    role: Role,
-    age: u8,
-}
-
-impl Member {
-    pub fn get_promotion() {}
-    pub fn new(name: String, role: Role, age: u8) -> Boss {
-        Member {
-            name: name.to_string(),
-            role: role,
-            age: age,
+    impl Member {
+        pub fn get_promotion() {}
+        pub fn new(name: String, role: Role, age: u8) -> Boss {
+            Member {
+                name: name.to_string(),
+                role: role,
+                age: age,
+            }
         }
     }
 }
