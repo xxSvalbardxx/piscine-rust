87f773528215b198e9e9acaed3f46698ad7f31b3
Svalbard <svalbardmalfant@gmail.com>
2023-06-12 19:33:06 +0200

    lalgebra_scalar

---
+++ lalgebra_scalar/src/lib.rs
@@ -0,0 +1,71 @@
+pub trait Scalar:
+    std::ops::Add<Output = Self>
+    + std::ops::Sub<Output = Self>
+    + std::ops::Mul<Output = Self>
+    + std::ops::Div<Output = Self>
+    + Sized
+{
+    type Item;
+    fn zero() -> Self::Item;
+    fn one() -> Self::Item;
+}
+
+impl Scalar for u32 {
+    type Item = u32;
+    fn zero() -> Self::Item {
+        0
+    }
+    fn one() -> Self::Item {
+        1
+    }
+}
+
+impl Scalar for u64 {
+    type Item = u64;
+    fn zero() -> Self::Item {
+        0
+    }
+    fn one() -> Self::Item {
+        1
+    }
+}
+
+impl Scalar for i32 {
+    type Item = i32;
+    fn zero() -> Self::Item {
+        0
+    }
+    fn one() -> Self::Item {
+        1
+    }
+}
+
+impl Scalar for i64 {
+    type Item = i64;
+    fn zero() -> Self::Item {
+        0
+    }
+    fn one() -> Self::Item {
+        1
+    }
+}
+
+impl Scalar for f32 {
+    type Item = f32;
+    fn zero() -> Self::Item {
+        0.0
+    }
+    fn one() -> Self::Item {
+        1.0
+    }
+}
+
+impl Scalar for f64 {
+    type Item = f64;
+    fn zero() -> Self::Item {
+        0.0
+    }
+    fn one() -> Self::Item {
+        1.0
+    }
+}
---
+++ lalgebra_scalar/src/main.rs
@@ -0,0 +1,8 @@
+use lalgebra_scalar::*;
+
+fn main() {
+	println!("{:?}", f64::zero());
+	println!("{:?}", i32::zero());
+	println!("{:?}", f64::one());
+	println!("{:?}", i32::one());
+}
