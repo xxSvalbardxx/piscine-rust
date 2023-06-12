11ca7e77f5673a4084656dd00fd68bbf29666a15
Svalbard <svalbardmalfant@gmail.com>
2023-06-12 15:55:14 +0200

    lifetimes

--- lifetimes/src/lib.rs
+++ lifetimes/src/lib.rs
@@ -1,11 +1,11 @@
 #[derive(Debug)]
-pub struct Person<'a> {
-	pub name: &'a str,
+pub struct Person<'a> { // <'a> is a lifetime annotation that says the struct cannot outlive the reference it holds
+	pub name: &'a mut str, // &str is a reference to a string slice (a string literal) that cannot be mutated (immutable)
 	pub age: u8,
 }
 
 impl Person<'_> {
-	pub fn new<'a>(name: &'a str) -> Person<'a>{
+	pub fn new<'a>(name: &'a mut str) -> Person<'a>{
 		Person {
 			name: name,
 			age: 0,
