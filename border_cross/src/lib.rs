f1e6f688d512dbc2140d0acc2c07ea2ab72123f0
Svalbard <svalbardmalfant@gmail.com>
2023-06-14 18:46:07 +0200

    border_cross

--- border_cross/src/lib.rs
+++ border_cross/src/lib.rs
@@ -42,7 +42,7 @@ impl Vehicle for Car<'_> {
     }
 }
 
-fn all_models(list: Vec<&dyn Vehicle>) -> Vec<&str> {
+pub fn all_models(list: Vec<&dyn Vehicle>) -> Vec<&str> {
     let mut models = Vec::new();
     for vehicle in list {
         models.push(vehicle.model());
