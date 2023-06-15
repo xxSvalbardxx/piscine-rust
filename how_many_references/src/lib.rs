87c1246b5c293f681d7972770b04d5133b82e234
Svalbard <svalbardmalfant@gmail.com>
2023-06-15 11:19:51 +0200

    how_many_references

---
+++ how_many_references/src/lib.rs
@@ -0,0 +1,21 @@
+pub use std::rc::Rc;
+
+pub struct Node {
+    pub ref_list: Vec<Rc<String>>,
+}
+
+impl Node {
+    pub fn new(ref_list: Vec<Rc<String>>) -> Node {
+        Node { ref_list: ref_list }
+    }
+    pub fn add_element(&mut self, element: Rc<String>) {
+        self.ref_list.push(element);
+    }
+    pub fn rm_all_ref(&mut self, element: Rc<String>) {
+        self.ref_list.retain(|x| x != &element); // retain() keeps all elements that satisfy the condition
+    }
+}
+
+pub fn how_many_references(ref_list: &Rc<String>) -> usize {
+    Rc::strong_count(ref_list)
+}
---
+++ how_many_references/src/main.rs
@@ -0,0 +1,25 @@
+use how_many_references::*;
+
+fn main() {
+    let a = Rc::new(String::from("a"));
+    let b = Rc::new(String::from("b"));
+    let c = Rc::new(String::from("c"));
+
+    let a1 = Rc::new(String::from("a"));
+
+    let mut new_node = Node::new(vec![a.clone()]);
+    new_node.add_element(b.clone());
+    new_node.add_element(a.clone());
+    new_node.add_element(c.clone());
+    new_node.add_element(a.clone());
+
+    println!("a: {:?}", how_many_references(&a));
+    println!("b: {:?}", how_many_references(&b));
+    println!("c: {:?}", how_many_references(&c));
+    new_node.rm_all_ref(a1.clone());
+    new_node.rm_all_ref(a.clone());
+
+    println!("a: {:?}", how_many_references(&a));
+    println!("b: {:?}", how_many_references(&b));
+    println!("c: {:?}", how_many_references(&c));
+}
