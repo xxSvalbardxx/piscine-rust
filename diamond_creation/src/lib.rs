b8f11e628d75b2361995a623b6c0f5a45ce91071
Svalbard <svalbardmalfant@gmail.com>
2023-06-21 17:13:41 +0200

    diamond_creation

--- diamond_creation/src/lib.rs
+++ diamond_creation/src/lib.rs
@@ -2,7 +2,7 @@ pub fn get_diamond(c: char) -> Vec<String> {
     let mut rslt = Vec::new();
     let n = (c as u8 - b'A') as usize;
 
-    for i in 0..n {
+    for i in 0..=n {
         let mut line : Vec<char> = vec![' '; 2*n+1];
         line[n-i] = (b'A' + i as u8 ) as char;
         line[n+i] = (b'A' + i as u8 ) as char;
