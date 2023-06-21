810ee7df70aaba599f0e1a582a54f5c02de7bfb8
Svalbard <svalbardmalfant@gmail.com>
2023-06-21 16:35:13 +0200

    ordinal

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -1,9 +1,9 @@
 pub fn num_to_ordinal(x: u32) -> String {
     let mut s = String::new();
     match x {
-        1 => s.push_str("st"),
-        2 => s.push_str("nd"),
-        3 => s.push_str("rd"),
+        1 => s.push_str(&format!("{}st", x)),
+        2 => s.push_str(&format!("{}nd", x)),
+        3 => s.push_str(&format!("{}rd", x)),
         _ => s.push_str(&format!("{}th", x)),
     }
     s
