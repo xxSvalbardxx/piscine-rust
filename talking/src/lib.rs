c3cd0667248287695a178fe8c1fa8c2b87ee8395
Svalbard <svalbardmalfant@gmail.com>
2023-06-22 15:09:02 +0200

    talking

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -4,7 +4,7 @@ pub fn talking(text: &str) -> String {
     let mut lowercase_count = 0;
     let mut uppercase_count = 0;
     let mut res = String::new();
-    for i in s.chars() {
+    for i in text.chars() {
         lowercase_count += match i {
             'a'..='z' => 1,
             _ => 0,
@@ -14,13 +14,13 @@ pub fn talking(text: &str) -> String {
             _ => 0,
         };
     }
-    if lowercase_count == 0 && uppercase_count != 0 && s.chars().nth(s.len()-1).unwrap() == '?' {
+    if lowercase_count == 0 && uppercase_count != 0 && text.chars().nth(text.len()-1).unwrap() == '?' {
         res = "Quiet, I am thinking!".to_string();
     } else if lowercase_count == 0 && uppercase_count != 0 {
         res = "There is no need to yell, calm down!".to_string();
-    } else if s.len() > 0 && s.chars().nth(s.len()-1).unwrap() == '?' {
+    } else if text.len() > 0 && s.chars().nth(text.len()-1).unwrap() == '?' {
         res = "Sure.".to_string();
-    } else if lowercase_count == 0 && uppercase_count == 0 || s.len() == 0 {
+    } else if lowercase_count == 0 && uppercase_count == 0 || text.len() == 0 {
         res = "Just say something!".to_string();
     } else {
         res = "Interesting".to_string();
