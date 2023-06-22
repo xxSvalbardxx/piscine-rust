09f96b44c71a1797486e131e0ae6268a1e6960c5
Svalbard <svalbardmalfant@gmail.com>
2023-06-22 12:40:25 +0200

    talking

---
+++ talking/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn talking(text: &str) -> &str {
+    if text.len() == 0 {
+        return "Just say something!";
+    } else if text.to_uppercase() == text && text.ends_with("?") && text.len() > 0 {
+        return "Quiet, I am thinking!";
+    } else if text.to_uppercase() == text && text.ends_with("!") && text.len() > 0 {
+        return "There is no need to yell, calm down!";
+    } else if text.ends_with("?") && text.len() > 0 {
+        return "Sure.";
+    } else {
+        return "Interesting.";
+    }
+}
+
+
---
+++ talking/src/main.rs
@@ -0,0 +1,9 @@
+use talking::*;
+
+fn main() {
+    println!("{:?}", talking("JUST DO IT!"));
+    println!("{:?}", talking("Hello how are you?"));
+    println!("{:?}", talking("WHAT'S GOING ON?"));
+    println!("{:?}", talking("something"));
+    println!("{:?}", talking(""));
+}
