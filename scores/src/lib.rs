57d25a9792fdf2b96fd36399aa8540c499742f3a
Svalbard <svalbardmalfant@gmail.com>
2023-06-22 10:07:37 +0200

    scores

---
+++ scores/src/lib.rs
@@ -0,0 +1,19 @@
+
+pub fn score(word: &str) -> u64 {
+    let mut total_score: u64 = 0;
+    for c in word.chars() {
+        let lowercase_char = c.to_lowercase().next().unwrap();
+        let score = match lowercasechar {
+            'a' | 'e' | 'i' | 'o' | 'u' | 'l' | 'n' | 'r' | 's' | 't' => 1,
+            'd' | 'g' => 2,
+            'b' | 'c' | 'm' | 'p' => 3,
+            'f' | 'h' | 'v' | 'w' | 'y' => 4,
+            'k' => 5,
+            'j' | 'x' => 8,
+            'q' | 'z' => 10,
+             => 0, // Handle any other characters that don't have a value
+        };
+        total_score += score;
+    }
+    total_score
+}
---
+++ scores/src/main.rs
@@ -0,0 +1,8 @@
+use scores::*;
+
+fn main() {
+    println!("{}", score("a"));
+    println!("{}", score("ã ê Á?"));
+    println!("{}", score("ThiS is A Test"));
+}
+
